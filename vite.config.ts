b1db81884780ee571ce0e26c9106345a62c3b587
경대 허 <hkd620@gmail.com>
2026-04-09 16:56:18 +0900

    fix: [접속 환경 보장] Chrome CORS 에러 격파 및 vite-plugin-mkcert 기반 안전한 로컬 HTTPS(5173) 환경 구축

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
+import mkcert from 'vite-plugin-mkcert';
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
+    mkcert()
   ],
-})
+  server: {
+    port: 5173,
+    strictPort: true,
+    cors: true,       // 도메인 불일치(CORS) 에러 완화
+    origin: 'https://localhost:5173', // Unsafe attempt 에러 방지용 리소스 출처 명시
+  }
+});
